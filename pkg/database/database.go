c31180f9c9ed59f260d059852e96caf8cc72023b
felipeAJdev <felipealexandrej@gmail.com>
2024-10-20 06:48:38 -0300

    fix: database connection

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -14,7 +14,7 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 
-	dsn := "root:root@tcp(127.0.0.1:3306)/tourism_app?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:33088)/hackathon_visitesp?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database!")
